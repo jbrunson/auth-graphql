86e19aa3f29bf18e38153247c812a109fa13fd2d
Jeff Brunson <jeff.a.brunson@gmail.com>
2017-03-12 15:37:09 -0600

    client actions

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -19,6 +19,25 @@ const mutation = new GraphQLObjectType({
         // returns a Promise so have to "return" it.
         return AuthService.signup({ email, password, req });
       }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        // Passport removes user property off req so we have to save a reference of it
+        req.logout();
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
+      }
     }
   }
 });
